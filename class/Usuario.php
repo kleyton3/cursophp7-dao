503d58869c4c16eaae0d0a339472493182407661
Kleyton Santos <kleytonkintas@gmail.com>
2021-08-04 17:52:53 +0100

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -56,7 +56,7 @@ class Usuario {
 		));
 
 		//if (isset($results[0]))
-	if (count($results) > 0) {
+		if (count($results) > 0) {
 
 		$row = $results[0];
 
@@ -65,10 +65,55 @@ class Usuario {
 		$this->setDessenha($row['dessenha']);
 		$this->setDtcadastro(new DateTime($row['dtcadastro']));
 
-	}
+	 	}
 
 
 	} 
+
+	public static function getList(){
+
+		$sql = new SQL();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+	public static function search($login){
+
+		$sql = new SQL();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+
+	}
+
+	public function login($login, $password){
+
+		$sql = new SQL();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		//if (isset($results[0]))
+		if (count($results) > 0) {
+
+		$row = $results[0];
+
+		$this->setIdusuario($row['idusuario']);
+		$this->setDeslogin($row['deslogin']);
+		$this->setDessenha($row['dessenha']);
+		$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+	 	} else {
+
+	 		throw new Exception("Login e ou Senha Inválidos");
+	 		
+	 	}
+
+	}
 
 	public function __toString() {
 
--- index.php
+++ index.php
@@ -8,10 +8,23 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
+/*Carrega um usuário
 $root = new Usuario();
-
 $root->loadById(3);
+echo $root;*/
 
-echo $root;
+/*Carrega uma lista de usuários
+$lista= Usuario::getList();
+echo json_encode($lista);*/
+
+/*Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario;
+$usuario->login("rafael", "!$%$");
+
+echo ($usuario);
 
 ?>
